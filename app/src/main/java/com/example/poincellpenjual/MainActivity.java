be3cf48d7bcbdbd52482bfa714b89d6bb6b412b7
Albi Saleh <helasibla2015@gmail.com>
2021-03-06 21:23:18 +0700

    commit_pertama

---
+++ app/src/main/java/com/example/poincellpenjual/MainActivity.java
@@ -0,0 +1,176 @@
+package com.example.poincellpenjual;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.blikoon.qrcodescanner.QrCodeActivity;
+import com.example.poincellpenjual.Server.Server;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_CODE_QR_SCAN = 101;
+
+    ImageView scanQr;
+
+    String id_konsumen, nama_konsumen, poin_konsumen;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        id_konsumen = "";
+        nama_konsumen = "";
+        poin_konsumen = "";
+
+        scanQr = findViewById(R.id.scan_qr);
+        scanQr.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Dexter.withContext(getApplicationContext())
+                        .withPermission(Manifest.permission.CAMERA)
+                        .withListener(new PermissionListener() {
+                            @Override
+                            public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+                                Intent i = new Intent(MainActivity.this, QrCodeActivity.class);
+                                startActivityForResult(i, REQUEST_CODE_QR_SCAN);
+                            }
+
+                            @Override
+                            public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+                                permissionDeniedResponse.getRequestedPermission();
+                            }
+
+                            @Override
+                            public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+
+                            }
+                        }).check();
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode != Activity.RESULT_OK) {
+            if (data == null)
+                return;
+            //Getting the passed result
+            String result = data.getStringExtra("com.blikoon.qrcodescanner.error_decoding_image");
+            if (result != null) {
+                AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
+                alertDialog.setTitle("Scan Error");
+                alertDialog.setMessage("QR Code gagal melakukan Scanner. Mohon Coba kembali.");
+                alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        });
+                alertDialog.show();
+            }
+            return;
+
+        }
+        if (requestCode == REQUEST_CODE_QR_SCAN) {
+            if (data == null)
+                return;
+            //Getting the passed result
+            String result = data.getStringExtra("com.blikoon.qrcodescanner.got_qr_scan_relult");
+            //Toast.makeText(this, result, Toast.LENGTH_SHORT).show();
+            //ambil_data(result);
+            AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
+            alertDialog.setTitle("Hasil Scan");
+            alertDialog.setMessage(result);
+            alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    });
+            alertDialog.show();
+
+        }
+    }
+
+    void ambil_data(String hasil_qrcode){
+
+        String URL_GET_DATA = "http://192.168.1.8/poincell/penjual/ambil_data.php?id_konsumen=" + hasil_qrcode;
+        Log.e("Ini Url", URL_GET_DATA);
+
+        /*JsonArrayRequest request = new JsonArrayRequest(URL_GET_DATA, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                Toast.makeText(MainActivity.this, "Data terbaca : ( " + response + ")", Toast.LENGTH_SHORT).show();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(MainActivity.this, "Data Error : ( " + error.getMessage() + ")", Toast.LENGTH_SHORT).show();
+            }
+        });*/
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, URL_GET_DATA,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        //Toast.makeText(MainActivity.this, "Data terbaca : ( " + response + ")", Toast.LENGTH_SHORT).show();
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            id_konsumen = jsonObject.getString("id_konsumen");
+                            nama_konsumen = jsonObject.getString("nama_konsumen");
+                            poin_konsumen = jsonObject.getString("poin_konsumen");
+
+                            Intent i = new Intent(MainActivity.this, )
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(MainActivity.this, "Data Error : ( " + error.getMessage() + ")", Toast.LENGTH_SHORT).show();
+                /*NetworkResponse statusCode = error.networkResponse;
+                System.out.println("Error : (" + statusCode + ")");*/
+            }
+        });
+        RequestQueue queue = Volley.newRequestQueue(this);
+        queue.add(stringRequest);
+    }
+}
---
+++ app/src/main/java/com/example/poincellpenjual/Server/Server.java
@@ -0,0 +1,6 @@
+package com.example.poincellpenjual.Server;
+
+public class Server {
+
+    public static final String URL = "http://192.168.0.83/poincell/penjual/";
+}
